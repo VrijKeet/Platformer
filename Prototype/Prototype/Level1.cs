cd19616890a912cda3181025e53bf7ae5c84289d
Maarten Keet <maarten.keet@me.com>
2013-03-03 22:12:04 +0100

    platformen level1 toevoegen

--- Prototype/Prototype/Level1.cs
+++ Prototype/Prototype/Level1.cs
@@ -23,7 +23,7 @@ namespace Prototype
         public Character character;
 
         public List<Platform> platforms;
-        public Vector2[] startPosPlat;
+        public List<Rectangle> startDimPlat;
         public List<Projectile> projectiles = new List<Projectile>();
 
 
@@ -44,54 +44,44 @@ namespace Prototype
             //testPlatform.boundingBox = new Rectangle(100, 100, 300, 100);
             platforms.Clear();
 
-            Platform platform = new Platform();
-            Platform platform2 = new Platform();
-            Platform platform3 = new Platform();
-            Platform platform4 = new Platform();
-            Platform platform5 = new Platform();
-            Platform platform6 = new Platform();
-            Platform platform7 = new Platform();
-            Platform platform8 = new Platform();
-            Platform platform9 = new Platform();
-            Platform platform10 = new Platform();
-            platforms.Add(platform);
-            platforms.Add(platform2);
-            platforms.Add(platform3);
-            platforms.Add(platform4);
-            platforms.Add(platform5);
-            platforms.Add(platform6);
-            platforms.Add(platform7);
-            platforms.Add(platform8);
-            platforms.Add(platform9);
-            platforms.Add(platform10);
+            // Lijst met posities van platformen
+            startDimPlat = new List<Rectangle>();
 
-            platforms[0].Initialize(grassTexture1);
-            platforms[1].Initialize(grassTexture1);
-            platforms[2].Initialize(grassTexture1);
-            platforms[3].Initialize(grassTexture1);
-            platforms[4].Initialize(grassTexture1);
-            platforms[5].Initialize(grassTexture1);
-            platforms[6].Initialize(grassTexture1);
-            platforms[7].Initialize(grassTexture1);
-            platforms[8].Initialize(grassTexture1);
-            platforms[9].Initialize(grassTexture1);
+            startDimPlat.Add(new Rectangle(0, 350, 2000, 350)); // Huge platform
+            startDimPlat.Add(new Rectangle(600, 250, 100, 100));
+            startDimPlat.Add(new Rectangle(1200, 250, 100, 100));
+            startDimPlat.Add(new Rectangle(1900, 250, 100, 100));
+            startDimPlat.Add(new Rectangle(750, 150, 400, 100)); // Groot platform onder
+            startDimPlat.Add(new Rectangle(800, 50, 90, 60));
+            startDimPlat.Add(new Rectangle(905, 50, 90, 60));
+            startDimPlat.Add(new Rectangle(1020, 50, 90, 60));
+            startDimPlat.Add(new Rectangle(700, -50, 500, 100)); // Groot platform boven
+            startDimPlat.Add(new Rectangle(600, -150, 200, 100)); // Wolk begin
+            startDimPlat.Add(new Rectangle(300, -250, 200, 100));
+            startDimPlat.Add(new Rectangle(0, -350, 200, 100));
+            startDimPlat.Add(new Rectangle(-300, -450, 200, 100)); // Wolk einde
+            startDimPlat.Add(new Rectangle(2100, 350, 500, 100));
+            startDimPlat.Add(new Rectangle(2800, 450, 300, 100));
+            startDimPlat.Add(new Rectangle(3300, 550, 300, 100));
+            startDimPlat.Add(new Rectangle(3850, 550, 1000, 350));
+            startDimPlat.Add(new Rectangle(4950, 550, 1000, 350));
 
+            // Platformen aanmaken
+            for (int i = 0; i < startDimPlat.Count; i++)
+            {
+                Platform platform = new Platform();
+                platforms.Add(platform);
+            }
 
-            startPosPlat = new Vector2[10] { new Vector2(233, 380), new Vector2(150, 290), new Vector2(350, 330), 
-                new Vector2(100, 430), new Vector2(-100, 250), new Vector2(600, 350), new Vector2(200, 200), 
-                new Vector2(200, 150), new Vector2(200, 100), new Vector2(200, 50) }; // Begin posities voor respawnen
-            platform.boundingBox = new Rectangle((int)startPosPlat[0].X, (int)startPosPlat[0].Y, 334, 100);
-            platform2.boundingBox = new Rectangle((int)startPosPlat[1].X, (int)startPosPlat[1].Y, 100, 10);
-            platform3.boundingBox = new Rectangle((int)startPosPlat[2].X, (int)startPosPlat[2].Y, 300, 15);
-            platform4.boundingBox = new Rectangle((int)startPosPlat[3].X, (int)startPosPlat[3].Y, 50, 10);
-            platform5.boundingBox = new Rectangle((int)startPosPlat[4].X, (int)startPosPlat[4].Y, 300, 10);
-            platform6.boundingBox = new Rectangle((int)startPosPlat[5].X, (int)startPosPlat[5].Y, 300, 10);
-            platform7.boundingBox = new Rectangle((int)startPosPlat[6].X, (int)startPosPlat[6].Y, 100, 10);
-            platform8.boundingBox = new Rectangle((int)startPosPlat[7].X, (int)startPosPlat[7].Y, 100, 10);
-            platform9.boundingBox = new Rectangle((int)startPosPlat[8].X, (int)startPosPlat[8].Y, 100, 10);
-            platform10.boundingBox = new Rectangle((int)startPosPlat[9].X, (int)startPosPlat[9].Y, 100, 10);
+            // Platformen texture geven
+            for (int i = 0; i < platforms.Count; i++)
+                platforms[i].Initialize(grassTexture1);
+
+            // Platformen posities geven
+            for (int i = 0; i < platforms.Count; i++)
+                platforms[i].boundingBox = startDimPlat[i];
             
-            Character.bounds = new Rectangle(300, 280, 80, 80);
+            Character.bounds = new Rectangle(300, platforms[0].boundingBox.Y - Game1.character1Texture.Height, 80, 80);
         }
 
 
